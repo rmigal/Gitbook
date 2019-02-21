b21c5c2107b358028d9d1f0edbd60a663ff67340
ruslanmyhal <migalruslann@gmail.com>
2019-02-21 21:50:51 +0200

    search ui

--- app/src/main/java/com/rusmyhal/gitbook/ui/MainActivity.kt
+++ app/src/main/java/com/rusmyhal/gitbook/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.rusmyhal.gitbook.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.rusmyhal.gitbook.R
+import com.rusmyhal.gitbook.ui.search.SearchFragment
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.mainContainer, SearchFragment())
+                .commitNow()
+        }
     }
 }
---
+++ app/src/main/java/com/rusmyhal/gitbook/ui/search/SearchFragment.kt
@@ -0,0 +1,11 @@
+package com.rusmyhal.gitbook.ui.search
+
+import com.rusmyhal.gitbook.R
+import com.rusmyhal.gitbook.ui.base.BaseFragment
+
+
+class SearchFragment : BaseFragment() {
+
+    override val layoutRes = R.layout.fragment_search
+
+}
