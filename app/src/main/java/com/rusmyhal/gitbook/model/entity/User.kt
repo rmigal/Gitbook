dd1a682448f494af79cd57b6da4307ca3ae294ec
ruslanmyhal <migalruslann@gmail.com>
2019-02-21 22:45:32 +0200

    create user entity

---
+++ app/src/main/java/com/rusmyhal/gitbook/model/entity/User.kt
@@ -0,0 +1,10 @@
+package com.rusmyhal.gitbook.model.entity
+
+import com.google.gson.annotations.SerializedName
+
+
+data class User(
+    @SerializedName("login") val login: String,
+    @SerializedName("id") val id: Long,
+    @SerializedName("gravatar_url") val gravatarUrl: String
+)
