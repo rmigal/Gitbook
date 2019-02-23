4dd95cf8049e385e087808d78e65b4051497b2cb
ruslanmyhal <migalruslann@gmail.com>
2019-02-23 23:58:07 +0200

    profile ui

---
+++ app/src/main/java/com/rusmyhal/gitbook/ui/profile/ProfileFragment.kt
@@ -0,0 +1,10 @@
+package com.rusmyhal.gitbook.ui.profile
+
+import com.rusmyhal.gitbook.R
+import com.rusmyhal.gitbook.ui.base.BaseFragment
+
+
+class ProfileFragment : BaseFragment() {
+
+    override val layoutRes = R.layout.fragment_profile
+}
